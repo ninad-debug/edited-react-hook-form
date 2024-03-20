9915db5de79aba8ef244a342231c3479ff91eb80
ninad-debug <ninadsamant07@gmail.com>
2024-03-20 18:40:54 +0530

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,56 @@
+import MuiTable from "../components/MuiTable"
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { removeUsername } from "../utils";
+import { useState } from "react";
+
+
+const HomePage = () => {
+
+  const [searchName, setSearchName] = useState('')
+  const navigate = useNavigate();
+
+  const handleAddUser = () => {
+    navigate('/adduser');
+  }
+
+  const handleLogout = () => {
+    removeUsername();
+    navigate("/signin");
+  }
+
+  const handleSearch = (event) => {
+    setSearchName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Welcome To User Data !</h2>
+      <Button style={{marginRight:"10px"}} variant="contained" size="small" startIcon={<PersonAddAltIcon />} onClick={handleAddUser}>
+        Add User
+      </Button>
+      
+      <Button variant="outlined" size="small" startIcon={<LogoutIcon />} onClick={handleLogout}>
+        Log Out
+      </Button>
+
+      
+      <br />
+      <br />
+      <label>Search- </label>
+      <input 
+      type="text"
+      value={searchName}
+      onChange={handleSearch}
+      />
+
+      <br />
+      <br />
+      <MuiTable searchData={searchName}/>
+    </div>
+  )
+}
+
+export default HomePage
